e8825ee01fefed5abf8a0877a121927cd343cd6b
chillsherlock <shubhampradhan806@gmail.com>
2026-04-10 14:45:05 +0530

    UC18:Linear Search for Bogie ID (Array-Based Searching)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,22 +1,39 @@
-// ===================== UC17 =====================
-// Sorting using Arrays.sort()
+// ===================== UC18 =====================
+// Linear Search for Bogie ID
 
-import java.util.Arrays;
+import java.util.Scanner;
 
-public class UC17_ArraysSort {
+public class UC18_LinearSearch {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
-        System.out.println("--- UC17: Built-in Sorting ---");
+        System.out.println("--- UC18: Linear Search ---");
 
-        // Array of bogie types
-        String[] bogies = {"Sleeper", "AC", "General", "ChairCar", "Luxury"};
+        // Array of bogie IDs
+        String[] bogieIds = {"B101", "B205", "B309", "B450", "B512"};
 
-        // Sorting
-        Arrays.sort(bogies);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Bogie ID to search: ");
+        String key = sc.nextLine();
 
-        // Display
-        System.out.println("Sorted Bogie Types: " + Arrays.toString(bogies));
+        boolean found = false;
+
+        // Linear Search
+        for (String id : bogieIds) {
+            if (id.equals(key)) {
+                found = true;
+                break; // early termination
+            }
+        }
+
+        // Result
+        if (found) {
+            System.out.println("Bogie Found");
+        } else {
+            System.out.println("Bogie Not Found");
+        }
+
+        sc.close();
     }
 }
