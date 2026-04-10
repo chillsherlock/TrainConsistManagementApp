e6684961bab744ae5cda54c43be6c90e656d6f15
chillsherlock <shubhampradhan806@gmail.com>
2026-04-10 14:12:06 +0530

    UC10:Count Total Seats in Train

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,8 +1,8 @@
-// ===================== UC9 =====================
-// Group Bogies using groupingBy()
+// ===================== UC10 =====================
+// Aggregate Capacity using Stream reduce()
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 // Bogie class
 class Bogie {
@@ -15,12 +15,12 @@ class Bogie {
     }
 }
 
-public class UC9_GroupBogies {
+public class UC10_TotalCapacity {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
-        System.out.println("--- UC9: Group Bogies ---");
+        System.out.println("--- UC10: Total Seating Capacity ---");
 
         // Create list
         List<Bogie> bogies = new ArrayList<>();
@@ -28,18 +28,12 @@ public class UC9_GroupBogies {
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 24));
 
-        // Group by capacity category
-        Map<String, List<Bogie>> grouped = bogies.stream()
-                .collect(Collectors.groupingBy(
-                        b -> (b.capacity > 50) ? "High Capacity" : "Low Capacity"
-                ));
+        // Stream aggregation
+        int total = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        // Display grouped result
-        for (String key : grouped.keySet()) {
-            System.out.println(key + ":");
-            for (Bogie b : grouped.get(key)) {
-                System.out.println("  " + b.name + " -> " + b.capacity);
-            }
-        }
+        // Display total
+        System.out.println("Total Seating Capacity: " + total);
     }
 }
